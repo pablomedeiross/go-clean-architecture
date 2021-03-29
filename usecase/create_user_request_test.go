31e0cf08b7d3ed4f34757fcdfda2794f29a92cb8
pablo medeiros <pablo_medeiros@sicredi.com.br>
2021-03-29 19:15:58 -0300

    Adjustment in test

--- usecase/create_user_request_test.go
+++ usecase/create_user_request_test.go
@@ -3,19 +3,19 @@ package usecase_test
 import (
 	"testing"
 	"user-api/usecase"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCreateUserRequestValidations(t *testing.T) {
 
-	names, emails, ages := 
+	names, emails, ages :=
 		[]string{"", "name1", "name2"},
 		[]string{"email1@gmail.com", "", "email1@gmail.com"},
-		[]int{13,16,0}
+		[]int{13, 16, 0}
 
-
-	request, err := usecase.NewCreateUserRequest(names[i],emails[i], ages[i])
+	_, err := usecase.NewCreateUserRequest(names[i], emails[i], ages[i])
 	assert.EqualError(t, err, "NewCreateUserRequest called without requested parameter")
 }
 
-// func TestName(t *testing.T) {} 
+// func TestName(t *testing.T) {}
