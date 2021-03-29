6232535b58e13acdd75ade4b14c104b9719f5bcd
pablo medeiros <pablo_medeiros@sicredi.com.br>
2021-03-29 19:09:17 -0300

    Changes in usecase

--- usecase/user_repository.go
+++
@@ -1,9 +0,0 @@
-package usecase
-
-import "user-api/entity/user"
-
-// UserRepository is an interface that represents the data persistent
-type UserRepository interface {
-	FindByName(name string) user.User
-	Save(user user.User) (user.User, error)
-}
