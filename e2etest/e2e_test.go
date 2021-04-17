fa17750b29f03c928397d090404c151da9fd72ca
Pablo <pabloalexandre2@hotmail.com>
2021-04-17 16:15:19 -0300

    Adding e2e test cenary to create user in api

--- e2etest/e2e_test.go
+++ e2etest/e2e_test.go
@@ -1,10 +1,23 @@
 package e2etest
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"user-api/e2etest/dto"
 	"user-api/external/configuration"
 	"user-api/external/db/memory"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+)
+
+const (
+	id_test       = "Id"
+	post          = "POST"
+	user_path     = "/users"
+	localhost_uri = "http://localhost:8080"
 )
 
 type E2ESuite struct {
@@ -20,17 +33,25 @@ func (suite *E2ESuite) SetupSuite() {
 
 	starter, _ := configuration.NewAppStarter("local")
 	suite.application = *starter
-	suite.application.Start()
+	go suite.application.Start()
 }
 
 func (suite *E2ESuite) TearDownSuite() {
 	suite.mongoDB.Stop()
 }
 
-// func (suite *E2ESuite) TestCool() {
-// 	assert.Equal(suite.T(), 5, 5)
-// }
+func TestE2ESuite(t *testing.T) {
+	suite.Run(t, new(E2ESuite))
+}
 
-// func TestE2ESuite(t *testing.T) {
-// 	suite.Run(t, new(E2ESuite))
-// }
+func (suite *E2ESuite) TestCreateUser() {
+
+	request := dto.RequestNewUser{Name: "name", Email: "email@gmail.com", Age: 12}
+	jsonRequest, _ := json.Marshal(request)
+
+	response, err := http.Post(localhost_uri+user_path, "application/json", bytes.NewReader(jsonRequest))
+
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), 201, response.StatusCode)
+	assert.Contains(suite.T(), response.Header.Get("Location"), localhost_uri+user_path+"/")
+}
