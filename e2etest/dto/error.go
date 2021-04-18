418ae1292704c2bcfe1dc09c59933fdaa5628ef7
Pablo <pabloalexandre2@hotmail.com>
2021-04-18 09:16:24 -0300

    Adding error cenary and other adjustments in e2eTest file

--- e2etest/dto/error.go
+++ e2etest/dto/error.go
@@ -1,1 +1,8 @@
 package dto
+
+type Error struct {
+	RequestPath  string `json:"request_path"`
+	RequestParms string `json:"request_params"`
+	RequestBody  string `json:"request_body"`
+	ErrorMsg     string `json:"error"`
+}
--- e2etest/e2e_test.go
+++ e2etest/e2e_test.go
@@ -3,8 +3,10 @@ package e2etest
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 	"user-api/e2etest/dto"
 	"user-api/external/configuration"
 	"user-api/external/db/memory"
@@ -17,6 +19,8 @@ const (
 	id_test       = "Id"
 	post          = "POST"
 	user_path     = "/users"
+	content_type  = "application/json"
+	location      = "Location"
 	localhost_uri = "http://localhost:8080"
 )
 
@@ -34,6 +38,7 @@ func (suite *E2ESuite) SetupSuite() {
 	starter, _ := configuration.NewAppStarter("local")
 	suite.application = *starter
 	go suite.application.Start()
+	time.Sleep(5000)
 }
 
 func (suite *E2ESuite) TearDownSuite() {
@@ -46,12 +51,42 @@ func TestE2ESuite(t *testing.T) {
 
 func (suite *E2ESuite) TestCreateUser() {
 
-	request := dto.RequestNewUser{Name: "name", Email: "email@gmail.com", Age: 12}
-	jsonRequest, _ := json.Marshal(request)
+	newUser := dto.RequestNewUser{Name: "name1", Email: "email@gmail.com", Age: 12}
+	jsonRequest, _ := json.Marshal(newUser)
 
-	response, err := http.Post(localhost_uri+user_path, "application/json", bytes.NewReader(jsonRequest))
+	response, err := http.Post(localhost_uri+user_path, content_type, bytes.NewReader(jsonRequest))
 
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), 201, response.StatusCode)
-	assert.Contains(suite.T(), response.Header.Get("Location"), localhost_uri+user_path+"/")
+	assert.Contains(suite.T(), response.Header.Get(location), localhost_uri+user_path+"/")
+}
+
+func (suite *E2ESuite) TestCreateUserThatAlreadyExists() {
+
+	newUser := dto.RequestNewUser{Name: "name2", Email: "email@gmail.com", Age: 12}
+	jsonRequest, _ := json.Marshal(newUser)
+	expectedError := dto.Error{
+		RequestPath:  user_path,
+		RequestParms: "",
+		RequestBody:  string(jsonRequest),
+		ErrorMsg: "Couldn't create new user, usecase returning error: " +
+			"The user already existis, user's name: " +
+			newUser.Name,
+	}
+
+	response, err := http.Post(localhost_uri+user_path, content_type, bytes.NewReader(jsonRequest))
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), 201, response.StatusCode)
+	assert.Contains(suite.T(), response.Header.Get(location), localhost_uri+user_path+"/")
+
+	actualError := &dto.Error{}
+	response, err = http.Post(localhost_uri+user_path, content_type, bytes.NewReader(jsonRequest))
+	assert.NoError(suite.T(), err)
+
+	defer response.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(bodyBytes, actualError)
+	assert.Equal(suite.T(), expectedError, *actualError)
+
 }
